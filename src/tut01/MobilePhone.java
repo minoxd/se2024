ab4a15702c2f0f2b8d8eff8b6d0fb29507b1cabf
minoxd <minophen.aceta114@gmail.com>
2024-03-25 17:04:12 +0700

    update

--- src/tut01/MobilePhone.java
+++ src/tut01/MobilePhone.java
@@ -1,6 +1,10 @@
 package tut01;
 
+import utils.AttrRef;
+import utils.DOpt;
 import utils.DomainConstraint;
+import utils.NotPossibleException;
+import utils.OptType;
 
 /**
  * @overview MobilePhone represents mobile phone
@@ -9,7 +13,7 @@ import utils.DomainConstraint;
  * 	model		String
  * 	color		Character	char
  * 	year		Integer		int
- * 	guaranteed	Boolean 
+ * 	guaranteed	boolean 
  * @object a typical MobilePhone object is mb=<mN, m, c, y> where manName(mN), model(m), color(c), year(y)
  * @abstract_properties
  * 	mutable(manName)=false /\ optional(manName)=false /\ length(manName)=10 /\ 
@@ -20,19 +24,253 @@ import utils.DomainConstraint;
  */
 public class MobilePhone {
 	//attributes
-	@DomainConstraint(mutable = false, optional = false, length = 10)
+	@DomainConstraint(type = "String", mutable = false, optional = false, length = LENGTH_MAN_NAME)
 	private String manName;
 	
-	@DomainConstraint(mutable = false, optional = false, length = 20)
+	@DomainConstraint(type = "String", mutable = false, optional = false, length = LENGTH_MODEL)
 	private String model;
 	
-	@DomainConstraint(mutable = false, optional = false)
+	@DomainConstraint(type = "Character", mutable = false, optional = false)
 	private char color;
 	
-	@DomainConstraint(mutable = false, optional = false, min = 1973, max = 2024)
+	@DomainConstraint(type = "Integer", mutable = false, optional = false, min = MIN_YEAR, max = MAX_YEAR)
 	private int year;
 	
-	@DomainConstraint(mutable = true, optional = true)
-	private Boolean guaranteed;
+	@DomainConstraint(type = "Boolean", mutable = true, optional = true)
+	private boolean guaranteed = false;
+
+	// constants
+	private static final int LENGTH_MAN_NAME = 10;
+	private static final int LENGTH_MODEL = 20;
+	private static final int MIN_YEAR = 1973;
+	private static final int MAX_YEAR = 2024;
 	
+	// constructor methods
+	/**
+	 * @effects <pre>
+	 * 	if manName, model, color, year are valid
+	 * 		initialise this as <manName, model, color, year>
+	 * 	else
+	 * 		initialise this as <> and inform error</pre>
+	 */
+	public MobilePhone(@AttrRef("manName") String manName,
+			@AttrRef("model") String model,
+			@AttrRef("color") char color,
+			@AttrRef("year") int year)
+			throws NotPossibleException {
+		if (!validateManName(manName)) {
+			throw new NotPossibleException("MobilePhone.init: Invalid mobile phone manufacturer name: " + manName);
+		}
+		
+		if (!validateModel(model)) {
+			throw new NotPossibleException("MobilePhone.init: Invalid mobile phone model: " + model);
+		}
+		
+		if (!validateColor(color)) {
+			throw new NotPossibleException("MobilePhone.init: Invalid mobile phone color: " + color);
+		}
+		
+		if (!validateYear(year)) {
+			throw new NotPossibleException("MobilePhone.init: Invalid mobile phone year: " + year);
+		}
+		
+		// initialise this as <manName, model, color, year>
+		this.manName = manName;
+		this.model = model;
+		this.color = color;
+		this.year = year;
+	}
+	
+	/**
+	 * @effects <pre>
+	 * 	if guaranteed is valid
+	 * 		set this.guaranteed=guaranteed
+	 * 		return true
+	 * 	else
+	 * 		return false</pre>
+	 */
+	@DOpt(type=OptType.Mutator)
+	@AttrRef("guaranteed")
+	public boolean setGuaranteed(boolean guaranteed) {
+		if (validateGuaranteed(guaranteed)) {
+			this.guaranteed = guaranteed;
+			return true;
+		}	else {
+			return false;
+		}
+	}
+	
+	/**
+	 * @effects return <tt>manName</tt>
+	 */
+	@DOpt(type=OptType.Observer)
+	@AttrRef("manName")
+	public String getManName() {
+		return manName;
+	}
+	
+	/**
+	 * @effects return <tt>model</tt>
+	 */
+	@DOpt(type=OptType.Observer)
+	@AttrRef("model")
+	public String getModel() {
+		return model;
+	}
+	
+	/**
+	 * @effects return <tt>color</tt>
+	 */
+	@DOpt(type=OptType.Observer)
+	@AttrRef("color")
+	public char getColor() {
+		return color;
+	}
+	
+	/**
+	 * @effects return <tt>year</tt>
+	 */
+	@DOpt(type=OptType.Observer)
+	@AttrRef("year")
+	public int getYear() {
+		return year;
+	}
+	
+	/**
+	 * @effects return <tt>guaranteed</tt>
+	 */
+	@DOpt(type=OptType.Observer)
+	@AttrRef("guaranteed")
+	public boolean getGuaranteed() {
+		return guaranteed;
+	}
+	
+	/**
+	 * @effects <pre>
+	 * 	if manName is valid
+	 * 		return true
+	 * 	else
+	 * 		return false</pre>
+	 */
+	private boolean validateManName(String manName) {
+		return (manName != null &&
+				manName.length() > 0 &&
+				manName.length() <= LENGTH_MAN_NAME);
+	}
+	
+	/**
+	 * @effects <pre>
+	 * 	if model is valid
+	 * 		return true
+	 * 	else
+	 * 		return false</pre>
+	 */
+	private boolean validateModel(String model) {
+		return (model != null &&
+				model.length() > 0 &&
+				model.length() <= LENGTH_MODEL);
+	}
+	
+	/**
+	 * @effects <pre>
+	 * 	if color is valid
+	 * 		return true
+	 * 	else
+	 * 		return false</pre>
+	 */
+	private boolean validateColor(char color) {
+		return (color == 'R' ||
+				color == 'O' ||
+				color == 'Y' ||
+				color == 'B' ||
+				color == 'P');
+	}
+	
+	/**
+	 * @effects <pre>
+	 * 	if year is valid
+	 * 		return true
+	 * 	else
+	 * 		return false</pre>
+	 */
+	private boolean validateYear(int year) {
+		return (year >= MIN_YEAR &&
+				year <= MAX_YEAR);
+	}
+	
+	/**
+	 * @effects <pre>
+	 * 	if guaranteed is valid
+	 * 		return true
+	 * 	else
+	 * 		return false</pre>
+	 */
+	private boolean validateGuaranteed(boolean guaranteed) {
+		return (guaranteed == true ||
+				guaranteed == false);
+	}
+	
+	@Override
+	public String toString() {
+		return "MobilePhone:<" + manName + 
+				", " + model + 
+				", " + color + 
+				", " + year + 
+				", " + guaranteed + 
+				">";
+	}
+	
+	/**
+	 * Java's specification (omitted)
+	 * 
+	 * Indicates whether some other object is "equal to" this one.
+	 * 
+	 * The equals method implements an equivalence relation on non-null object references:
+	 * <ul>
+	 *   <li>It is reflexive: for any non-null reference value x, x.equals(x) should return true.
+	 *   <li>It is symmetric: for any non-null reference values x and y, x.equals(y) should return true if and only if y.equals(x) returns true.
+	 *   <li>It is transitive: for any non-null reference values x, y, and z, if x.equals(y) returns true and y.equals(z) returns true, then x.equals(z) should return true.
+	 *   <li>It is consistent: for any non-null reference values x and y, multiple invocations of x.equals(y) consistently return true or consistently return false, provided no information used in equals comparisons on the objects is modified.
+	 * </ul>
+	 * 
+	 * For any non-null reference value x, x.equals(null) should return false.
+	 * The equals method for class Object implements the most discriminating possible equivalence relation on objects; 
+	 *  that is, for any non-null reference values x and y, 
+	 *  this method returns true if and only if x and y refer to the same object 
+	 *    (x == y has the value true).
+	 * 
+	 * Note that it is generally necessary to override the hashCode method whenever this method is overridden, 
+	 * so as to maintain the general contract for the hashCode method, 
+	 * which states that equal objects must have equal hash codes.
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof MobilePhone)) 
+			return false;
+
+		String yourManName = ((MobilePhone) o).manName;
+		String yourModel = ((MobilePhone) o).model;
+		char yourColor = ((MobilePhone) o).color;
+		int yourYear = ((MobilePhone) o).year;
+		boolean yourGuaranteed = ((MobilePhone) o).guaranteed;
+		return yourManName == manName &&
+				yourModel == model &&
+				yourColor == color &&
+				yourYear == year &&
+				yourGuaranteed == guaranteed;
+	}
+	
+	/**
+	 * @effects <pre>
+	 * 	if this satisfies abstract properties
+	 * 		return true
+	 * 	else
+	 * 		return false</pre>
+	 */
+	public boolean repOK() {
+		return validateManName(manName) &&
+				validateModel(model) &&
+				validateColor(color) &&
+				validateYear(year);
+	}
 }
--- src/tut01/Person.java
+++ src/tut01/Person.java
@@ -1,6 +1,10 @@
 package tut01;
 
+import utils.AttrRef;
+import utils.DOpt;
 import utils.DomainConstraint;
+import utils.NotPossibleException;
+import utils.OptType;
 
 /**
  * @overview Person represents someone who join into the seminar
@@ -13,15 +17,215 @@ import utils.DomainConstraint;
  * 	mutable(id)=false /\ optional(id)=false /\ min(id)=1 /\
  * 	mutable(name)=true /\ optional(name)=false /\ length(name)=50 /\
  * 	mutable(phone)=true /\ optional(phone)=true /\ 
+ * @author minoxd
  */
 public class Person {
 	// attributes
-	@DomainConstraint(mutable = false, optional = false, min = 1)
+	@DomainConstraint(type = "Integer", mutable = false, optional = false, min = MIN_ID)
 	private int id;
 	
-	@DomainConstraint(mutable = true, optional = false, length = 50)
+	@DomainConstraint(type = "String", mutable = true, optional = false, length = LENGTH_NAME)
 	private String name;
 	
-	@DomainConstraint(mutable = true, optional = true)
+	@DomainConstraint(type = "MobilePhone", mutable = true, optional = true)
 	private MobilePhone phone;
+	
+	// constants
+	private static final int MIN_ID = 1;
+	private static final int LENGTH_NAME = 50;
+	
+	// constructor methods
+	/**
+	 * @effects <pre>
+	 * 	if id, name are valid
+	 * 		initialise this as <id, name>
+	 * 	else
+	 * 		initialise this as <> and inform error</pre>
+	 */
+	public Person(@AttrRef("id") int id, @AttrRef("name") String name)
+			throws NotPossibleException {
+		if (!validateId(id)) {
+			throw new NotPossibleException("Person.init: Invalid person id: " + id);
+		}
+		
+		if (!validateName(name)) {
+			throw new NotPossibleException("Person.init: Invalid person name: " + name);
+		}
+		
+		// initialise this as <id, name>
+		this.id = id;
+		this.name = name;
+	}
+	
+	/**
+	 * @effects <pre>
+	 * 	if name is valid
+	 * 		set this.name=name
+	 * 		return true
+	 * 	else
+	 * 		return false</pre>
+	 */
+	@DOpt(type=OptType.Mutator)
+	@AttrRef("name")
+	public boolean setName(String name) {
+		if (validateName(name)) {
+			this.name = name;
+			return true;
+		}	else {
+			return false;
+		}
+	}
+	
+	/**
+	 * @effects <pre>
+	 * 	if phone is valid
+	 * 		set this.phone=phone
+	 * 		return true
+	 * 	else
+	 * 		return false</pre>
+	 */
+	@DOpt(type=OptType.Mutator)
+	@AttrRef("phone")
+	public boolean setPhone(MobilePhone phone) {
+		if (phone.repOK()) {
+			this.phone = phone;
+			return true;
+		}	else {
+			return false;
+		}
+	}
+	
+	/**
+	 * @effects return <tt>id</tt>
+	 */
+	@DOpt(type=OptType.Observer)
+	@AttrRef("id")
+	public int getId() {
+		return id;
+	}
+	
+	/**
+	 * @effects return <tt>name</tt>
+	 */
+	@DOpt(type=OptType.Observer)
+	@AttrRef("name")
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @effects return <tt>phone</tt>
+	 */
+	@DOpt(type=OptType.Observer)
+	@AttrRef("phone")
+	public MobilePhone getPhone() {
+		return phone;
+	}
+	
+	/**
+	 * @effects <pre>
+	 * 	if id is valid
+	 * 		return true
+	 * 	else
+	 * 		return false</pre>
+	 */
+	private boolean validateId(int id) {
+		return id >= MIN_ID;
+	}
+	
+	/**
+	 * @effects <pre>
+	 * 	if name is valid
+	 * 		return true
+	 * 	else
+	 * 		return false</pre>
+	 */
+	private boolean validateName(String name) {
+		return (name != null &&
+				name.length() > 0 &&
+				name.length() <= LENGTH_NAME);
+	}
+	
+	@Override
+	public String toString() {
+		return "Person: <" + id + 
+				", " + name + 
+				", " + phone.toString() + 
+				">";
+	}
+	
+	/**
+	 * Java's specification (omitted)
+	 * 
+	 * Indicates whether some other object is "equal to" this one.
+	 * 
+	 * The equals method implements an equivalence relation on non-null object references:
+	 * <ul>
+	 *   <li>It is reflexive: for any non-null reference value x, x.equals(x) should return true.
+	 *   <li>It is symmetric: for any non-null reference values x and y, x.equals(y) should return true if and only if y.equals(x) returns true.
+	 *   <li>It is transitive: for any non-null reference values x, y, and z, if x.equals(y) returns true and y.equals(z) returns true, then x.equals(z) should return true.
+	 *   <li>It is consistent: for any non-null reference values x and y, multiple invocations of x.equals(y) consistently return true or consistently return false, provided no information used in equals comparisons on the objects is modified.
+	 * </ul>
+	 * 
+	 * For any non-null reference value x, x.equals(null) should return false.
+	 * The equals method for class Object implements the most discriminating possible equivalence relation on objects; 
+	 *  that is, for any non-null reference values x and y, 
+	 *  this method returns true if and only if x and y refer to the same object 
+	 *    (x == y has the value true).
+	 * 
+	 * Note that it is generally necessary to override the hashCode method whenever this method is overridden, 
+	 * so as to maintain the general contract for the hashCode method, 
+	 * which states that equal objects must have equal hash codes.
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof Person))
+			return false;
+		
+		int yourID = ((Person) o).id;
+		return yourID == id;
+	}
+	
+	/**
+	 * Java's specification (omitted):
+	 * 
+	 * <div>Returns a hash code value for the object. 
+	 * 
+	 * This method is supported for the benefit of hash tables 
+	 * such as those provided by java.util.HashMap.
+	 * 
+	 * <div>The <b>general contract</b> of hashCode is:
+	 * 
+	 * <div>Whenever it is invoked on the same object more than once during an execution of a 
+	 * Java application, the hashCode method must consistently return the same integer, 
+	 * provided no information used in equals comparisons on the object is modified. 
+	 * This integer need not remain consistent from one execution of an application 
+	 * to another execution of the same application.
+	 * 
+	 * <div>If two objects are equal according to the equals(Object) method, 
+	 * then calling the hashCode method on each of the two objects must produce the 
+	 * same integer result.
+	 * 
+	 * <div>It is not required that if two objects are unequal according to the 
+	 * java.lang.Object.equals(java.lang.Object) method, 
+	 * then calling the hashCode method on each of the two objects must 
+	 * produce distinct integer results. 
+	 * However, the programmer should be aware that producing distinct integer 
+	 * results for unequal objects may improve the performance of hash tables.
+	 */
+	@Override
+	public int hashCode() {
+		return id;
+	}
+	
+	/**
+	 * @effects <pre>
+	 * 	if this satisfies abstract properties
+	 * 		return true
+	 * 	else
+	 * 		return false</pre>
+	 */
+	public boolean repOK() {
+		return validateId(id) && validateName(name);
+	}
 }
