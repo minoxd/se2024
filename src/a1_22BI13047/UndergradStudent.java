f27664dcc5567b9d86fc6a8c03586b87eb15321b
minoxd <minophen.aceta114@gmail.com>
2024-04-06 09:02:31 +0700

    update

---
+++ src/a1_22BI13047/UndergradStudent.java
@@ -0,0 +1,12 @@
+package a1_22BI13047;
+
+import utils.NotPossibleException;
+
+public class UndergradStudent extends Student{
+
+	public UndergradStudent(double id, String name, String phoneNumber, String address) throws NotPossibleException {
+		super(id, name, phoneNumber, address);
+		// TODO Auto-generated constructor stub
+	}
+
+}
